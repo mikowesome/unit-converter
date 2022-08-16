3403ab865b723d89e06e64de8e35446db871b3cb
Miko <jm_mapilisan@yahoo.com>
2022-08-16 22:12:16 +0800

    Add warning when inputing falsy values

--- script.js
+++ script.js
@@ -11,22 +11,27 @@ convertBtn.addEventListener('click', () => convert())
 // Functions
 function convert() {
     let num = Number(input.value)
-    let lengthFeet = (num * 3.281).toFixed(3)
-    let lengthMeter = (num / 3.281).toFixed(3)
-    let volumeGallon = (num * 0.264).toFixed(3)
-    let volumeLiter = (num / 0.264).toFixed(3)
-    let massPound = (num * 2.204).toFixed(3)
-    let massKilogram = (num / 2.204).toFixed(3)
-    paraLength.innerHTML = `
-    ${input.value} meters = ${lengthFeet} feet | 
-    ${input.value} feet = ${lengthMeter} meters
-    `
-    paraVolume.innerHTML = `
-    ${input.value} liters = ${volumeGallon} gallons | 
-    ${input.value} gallons = ${volumeLiter} liters
-    `
-    paraMass.innerHTML = `
-    ${input.value} kilos = ${massPound} pounds | 
-    ${input.value} pounds = ${massKilogram} kilos
-    `
+    if (num) {
+        let lengthFeet = (num * 3.281).toFixed(3)
+        let lengthMeter = (num / 3.281).toFixed(3)
+        let volumeGallon = (num * 0.264).toFixed(3)
+        let volumeLiter = (num / 0.264).toFixed(3)
+        let massPound = (num * 2.204).toFixed(3)
+        let massKilogram = (num / 2.204).toFixed(3)
+        paraLength.innerHTML = `
+        ${input.value} meters = ${lengthFeet} feet | 
+        ${input.value} feet = ${lengthMeter} meters
+        `
+        paraVolume.innerHTML = `
+        ${input.value} liters = ${volumeGallon} gallons | 
+        ${input.value} gallons = ${volumeLiter} liters
+        `
+        paraMass.innerHTML = `
+        ${input.value} kilos = ${massPound} pounds | 
+        ${input.value} pounds = ${massKilogram} kilos
+        `
+    } else {
+        alert("Please input a positive integer.")
+    }
+
 }
